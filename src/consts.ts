6cf51209ffce0f7ae8dc165e386e8e6c9119f23f
Li Lin <84984903@qq.com>
2020-08-09 21:38:32 +0800

    同时只能生成1个路书

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
 export const SCREENSHOTS_DIR_BASE = "./screenshots";
 export const PAGE_SIZE: [number, number] = [2256, 3024];
+export const PROCESS_KEY = 'PROCESS_KEY';
--- src/controller/homeController.ts
+++ src/controller/homeController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import getPdf from "../service/pdfService";
 import cache from "../cache";
+import { PROCESS_KEY } from '../consts';
+
 
 const index = async (req: Request, res: Response): Promise<void> => {
   req.setTimeout(60 * 20 * 1000);
@@ -10,27 +12,29 @@ const index = async (req: Request, res: Response): Promise<void> => {
     const url = query["url"] as string;
 
     if(!url || url.length === 0) {
-      res.status(500).json({ error: "缺少参数url" });
+      res.status(500).json({ message: "缺少参数url" });
       return;
     }
 
-    if(cache.get(url)) {
-      res.status(201).json({ message: "已经在处理该页面" });
+    if(cache.get(PROCESS_KEY)) {
+      res.status(405).json({ message: "同一时间只能生成一份路书，请稍后重试" });
       return;
     }
 
-    cache.set(url, true);
+    cache.set(PROCESS_KEY, true);
 
     console.log("pdf request received url:", url);
 
     const result = await getPdf(url, res);
 
+    cache.del(PROCESS_KEY);
+
     if (!result) {
-      res.status(500).json({ error: "没有页面需要生成" });
+      res.status(405).json({ message: "没有页面需要生成" });
     }
   } catch (ex) {
     console.log("err:", ex);
-    res.status(500).json({ error: ex });
+    res.status(500).json({ message: ex });
   }
 };
 
