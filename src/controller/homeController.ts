f31536b916946db326f1e84e7e8f1a7f7d3fe6bc
Li Lin <84984903@qq.com>
2020-08-09 21:54:22 +0800

    FIX 同一时间只能生成1个路书

--- src/controller/homeController.ts
+++ src/controller/homeController.ts
@@ -6,28 +6,27 @@ import { PROCESS_KEY } from '../consts';
 
 const index = async (req: Request, res: Response): Promise<void> => {
   req.setTimeout(60 * 20 * 1000);
+
+  const { query } = req;
+
+  const url = query["url"] as string;
+
+  console.log("pdf request received url:", url);
+
+  if (!url || url.length === 0) {
+    res.status(500).json({ message: "缺少参数url" });
+    return;
+  }
+
+  if (cache.get(PROCESS_KEY)) {
+    res.status(405).json({ message: "同一时间只能生成一份路书，请稍后重试" });
+    return;
+  }
+
+  cache.set(PROCESS_KEY, true);
+
   try {
-    const { query } = req;
-
-    const url = query["url"] as string;
-
-    if(!url || url.length === 0) {
-      res.status(500).json({ message: "缺少参数url" });
-      return;
-    }
-
-    if(cache.get(PROCESS_KEY)) {
-      res.status(405).json({ message: "同一时间只能生成一份路书，请稍后重试" });
-      return;
-    }
-
-    cache.set(PROCESS_KEY, true);
-
-    console.log("pdf request received url:", url);
-
     const result = await getPdf(url, res);
-
-    cache.del(PROCESS_KEY);
 
     if (!result) {
       res.status(405).json({ message: "没有页面需要生成" });
@@ -35,6 +34,8 @@ const index = async (req: Request, res: Response): Promise<void> => {
   } catch (ex) {
     console.log("err:", ex);
     res.status(500).json({ message: ex });
+  } finally {
+    cache.del(PROCESS_KEY);
   }
 };
 
